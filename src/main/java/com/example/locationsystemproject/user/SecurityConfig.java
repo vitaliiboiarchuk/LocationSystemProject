45bfad55a96615bb0839703c38dcc7e3548e1b50
vitaliiboiarchuk <boyarchuk.vitaliy@gmail.com>
2023-03-01 13:55:29 +0100

    added security config

---
+++ src/main/java/com/example/locationsystemproject/user/SecurityConfig.java
@@ -0,0 +1,29 @@
+package com.example.locationsystemproject.user;
+
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.security.config.annotation.method.configuration.EnableMethodSecurity;
+import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
+import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
+import org.springframework.security.web.SecurityFilterChain;
+
+@Configuration
+@EnableWebSecurity
+@EnableMethodSecurity(securedEnabled = true)
+public class SecurityConfig {
+
+    @Bean
+    public BCryptPasswordEncoder passwordEncoder() {
+        return new BCryptPasswordEncoder();
+    }
+
+    @Bean
+    protected SecurityFilterChain configure(HttpSecurity http) throws Exception {
+        http.authorizeHttpRequests()
+                .requestMatchers("/**").permitAll()
+                .and().formLogin().loginPage("/login")
+                .and().logout().logoutSuccessUrl("/");
+        return http.build();
+    }
+}
