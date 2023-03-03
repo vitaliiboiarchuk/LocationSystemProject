f14f5782e51610e32dc0174f4cc8d93fefa8bbe0
vitaliiboiarchuk <boyarchuk.vitaliy@gmail.com>
2023-03-03 12:14:00 +0100

    added should find user by username test

--- src/test/groovy/com/example/locationsystemproject/user/UserControllerTest.groovy
+++ src/test/groovy/com/example/locationsystemproject/user/UserControllerTest.groovy
@@ -1,17 +1,13 @@
 package com.example.locationsystemproject.user
 
 import org.mockito.InjectMocks
-import org.mockito.Mock
-import org.mockito.Mockito
 import org.springframework.beans.factory.annotation.Autowired
 import org.springframework.boot.test.autoconfigure.web.servlet.AutoConfigureMockMvc
 import org.springframework.boot.test.context.SpringBootTest
-import org.springframework.boot.test.mock.mockito.MockBean
 import org.springframework.test.web.servlet.MockMvc
 import spock.lang.Specification
 
 import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.get
-import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.post
 import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.model
 import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.status
 import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.view
---
+++ src/test/groovy/com/example/locationsystemproject/user/UserServiceTest.groovy
@@ -0,0 +1,46 @@
+package com.example.locationsystemproject.user
+
+import org.junit.jupiter.api.extension.ExtendWith
+import org.mockito.Mockito
+import org.springframework.beans.factory.annotation.Autowired
+import org.springframework.boot.test.context.SpringBootTest
+import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder
+import org.springframework.test.context.junit.jupiter.SpringExtension
+import spock.lang.Specification
+
+@ExtendWith(SpringExtension.class)
+@SpringBootTest
+class UserServiceTest extends Specification {
+
+    @Autowired
+    UserService userService
+
+    @Autowired
+    UserRepository userRepository
+
+    @Autowired
+    BCryptPasswordEncoder bCryptPasswordEncoder
+
+    @Autowired
+    RoleRepository roleRepository
+
+    void setup() {
+        userRepository = Mockito.mock(UserRepository.class)
+        bCryptPasswordEncoder = new BCryptPasswordEncoder()
+        roleRepository = Mockito.mock(RoleRepository.class)
+        userService = new UserServiceImpl(userRepository,roleRepository,bCryptPasswordEncoder)
+    }
+
+    def "should find user by username"() {
+        given: "set the data for method testing"
+        def user1 = new User()
+        user1.setUsername("boyarchuk.vitaliy@gmail.com")
+        Mockito.when(userRepository.findByUsername("boyarchuk.vitaliy@gmail.com")).thenReturn(user1)
+
+        when: "calling the testing method"
+        User user = userService.findByUserName("boyarchuk.vitaliy@gmail.com")
+
+        then: "comparing expected with actual"
+        user.getUsername() == "boyarchuk.vitaliy@gmail.com"
+    }
+}
