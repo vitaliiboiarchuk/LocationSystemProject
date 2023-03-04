0dd87ee563028d9627a4bc837ee6cb4dd92c3032
vitaliiboiarchuk <boyarchuk.vitaliy@gmail.com>
2023-03-04 15:05:33 +0100

    added location repo test

--- src/test/groovy/com/example/locationsystemproject/location/LocationRepositoryTest.groovy
+++ src/test/groovy/com/example/locationsystemproject/location/LocationRepositoryTest.groovy
@@ -21,27 +21,85 @@ class LocationRepositoryTest extends Specification {
     def location = new Location()
     def user = new User()
 
+    def user2 = new User()
+    def readOnlyLocation = new Location()
+    List<Location> readOnlyLocations = new ArrayList<>()
+
+    def adminLocation = new Location()
+    List<Location> adminLocations = new ArrayList<>()
 
     void setup() {
         user.setUsername("test")
-        user.setId(1L)
         user.setPassword("test")
         user.setName("test")
         user.setEnabled(1)
-        testEntityManager.merge(user)
+
+        user2.setName("test")
+        user2.setEnabled(1)
+        user2.setPassword("test")
+        user2.setUsername("grgrgrg@gmail.com")
+        testEntityManager.merge(user2)
+
         location.setName("test")
         location.setAddress("test")
-        location.setUser(user)
-        testEntityManager.merge(location)
+
+        readOnlyLocation.setName("test")
+        readOnlyLocation.setAddress("test")
+
+        adminLocation.setName("test")
+        adminLocation.setAddress("test")
     }
 
+
     def "should find all locations that user added"() {
+        given: "set the data for method testing"
+        user.setId(1L)
+        testEntityManager.merge(user)
+        location.setUser(user)
+        testEntityManager.merge(location)
+
         when: "calling the testing method"
         List<Location> result = locationRepository.findAllMyLocations(location.user.getId())
 
         then: "comparing expected with actual"
         for (Location loc : result) {
             loc.user.getId() == user.getId()
+        }
+    }
+
+    def "should find all locations with read only access that user has"() {
+        given: "set the data for method testing"
+        user.setId(2L)
+        testEntityManager.merge(user)
+        readOnlyLocation.setUser(user)
+        testEntityManager.merge(readOnlyLocation)
+        readOnlyLocations.add(readOnlyLocation)
+        user2.setReadOnlyLocations(readOnlyLocations)
+
+        when: "calling the testing method"
+        List<Location> result = locationRepository.findAllMyReadOnlyLocations(user2.getId())
+
+        then: "comparing expected with actual"
+        for (Location loc : result) {
+            loc == readOnlyLocation
+        }
+    }
+
+    def "should find all locations with admin access that user has"() {
+        given: "set the data for method testing"
+        user.setId(3L)
+        testEntityManager.merge(user)
+        adminLocation.setUser(user)
+        testEntityManager.merge(adminLocation)
+        adminLocations.add(adminLocation)
+        user2.setAdminLocations(adminLocations)
+
+        when: "calling the testing method"
+        List<Location> result = locationRepository.findAllMyAdminLocations(user2.getId())
+
+        then: "comparing expected with actual"
+        for (Location loc : result) {
+            loc == adminLocation
         }
     }
 }
